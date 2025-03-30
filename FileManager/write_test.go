7583d44a92422bb75910727f3cbd1aa486e10772
Rezishon <rezashahmoradi.81@outlook.com>
2025-03-30 23:08:40 +0330

    test: add unit test for "write" file
    All functions now have functions for unit test.
    There is two variables for "samplePath", & "sampleMessage". These
    contains sample values which could change in each tests, but changes
    doesn't make error by this global variables.
    How does them work:
    The unit will call the function & log the return value. Then check that
    the wanted file is exist or not. At the end it will handle the error and
    log the result. If the result was succesful, the sample file will
    remove.

---
+++ FileManager/write_test.go
@@ -0,0 +1,66 @@
+package FileManager
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var samplePath string = "../test/SampleCodes.md"
+var sampleMessage string = "this is a sample data"
+
+func TestOverwriteFile(t *testing.T) {
+	t.Log(OverwriteFile(samplePath, []byte(sampleMessage)))
+
+	_, err := os.Stat(samplePath)
+
+	if err != nil {
+		t.Log("Oh fucked: %w", err)
+	} else {
+		t.Log("Fine, file exist")
+		os.Remove(samplePath)
+	}
+}
+
+func TestOverwriteFileString(t *testing.T) {
+	t.Log(OverwriteFileString(samplePath, sampleMessage))
+
+	_, err := os.Stat(samplePath)
+
+	if err != nil {
+		t.Log("Oh fucked: %w", err)
+	} else {
+		t.Log("Fine, file exist")
+		os.Remove(samplePath)
+	}
+}
+
+func TestAppendFile(t *testing.T) {
+	t.Log(AppendFile(samplePath, []byte(sampleMessage)))
+
+	_, err := os.Stat(samplePath)
+
+	if err != nil {
+		t.Log("Oh fucked: %w", err)
+	} else {
+		t.Log("Fine, file exist")
+		os.Remove(samplePath)
+	}
+}
+
+func TestAppendFileString(t *testing.T) {
+	t.Log(AppendFileString(samplePath, sampleMessage))
+
+	_, err := os.Stat(samplePath)
+
+	if err != nil {
+		t.Log("Oh fucked: %w", err)
+	} else {
+		t.Log("Fine, file exist")
+		os.Remove(samplePath)
+	}
+}
+
+func TestErrorHandler(t *testing.T) {
+	t.Log(ErrorHandler(fmt.Errorf("This is a error message"), "Oh we made an error"))
+}
