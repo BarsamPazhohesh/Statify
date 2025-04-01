26cb6c20dfa000861ad5170da0666ab77cd04544
rezishon <rezashahmoradi.81@outlook.com>
2025-04-01 23:53:19 +0330

    test: add unit test for `DatabaseManager`
    I add a simple unit test for functions that I wrote earlier.
    The `existenceChacker` function should check the tables structure too,
    but I only check wheter the database file built or not.

---
+++ Database/databaseManager_test.go
@@ -0,0 +1,25 @@
+package Database
+
+import (
+	"fmt"
+	"statfiy/FileManager"
+	"testing"
+)
+
+func TestCreateFileMetadataTable(t *testing.T) {
+	t.Log(createFileMetadataTable())
+	existenceChecker(t)
+
+}
+
+func TestCreateAnalyzeFileResultTable(t *testing.T) {
+	t.Log(createAnalyzeFileResultTable())
+	existenceChecker(t)
+}
+
+
+func existenceChecker(t *testing.T) {
+	existence := FileManager.IsFileExists(DatabasePath)
+	//! should check the database table
+	t.Log("IsFileExists: ", existence)
+}
