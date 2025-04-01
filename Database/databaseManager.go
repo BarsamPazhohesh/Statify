0a1f96f592f708472571160cc33d932f0f6b7714
rezishon <rezashahmoradi.81@outlook.com>
2025-04-01 23:22:19 +0330

    feat: add global variables
    Global variables Contain things that may need to change after production
    step. Things like `driverName`, `DatabaseName`, & etc.
    Assigning values to variables will accure in `init()`.

--- Database/databaseManager.go
+++ Database/databaseManager.go
@@ -6,3 +6,13 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 )
+var driverName string
+var DatabasePath string
+var analyzeFileResultTableName string
+var fileMetadataTableName string
+func init() {
+	driverName = "sqlite3"
+	DatabasePath = "./StatifyDatabase.db"
+	fileMetadataTableName = "TblFileMetadata"
+	analyzeFileResultTableName = "TblAnalyzeFileResult"
+}
