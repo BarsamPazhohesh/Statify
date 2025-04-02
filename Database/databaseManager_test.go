b0a5f4df7c0cbf0fb9213aa7cfd4fcc038652f0b
rezishon <rezashahmoradi.81@outlook.com>
2025-04-03 02:24:51 +0330

    test: add unit test for query builders

--- Database/databaseManager_test.go
+++ Database/databaseManager_test.go
@@ -11,10 +11,24 @@ func TestCreateFileMetadataTable(t *testing.T) {
 	err := createFileMetadataTable()
 	require.NoError(t, err, "Something messed up")
 }
+func TestFileMetadataQueryText(t *testing.T) {
+	err := fileMetadataQueryText(fileMetadataTableName, primaryKeyAttribute{AttributeName: "id", Type: "INTEGER"})
+	require.Equal(t,
+		"CREATE TABLE IF NOT EXISTS TblFileMetadata (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \n\tName TEXT, \n\tPath TEXT, \n\tDir TEXT, \n\tExtension TEXT, \n\tSize int, \n\tModifiedAt TIMESTAMP)",
+		err,
+		"Something messed up")
+}
 
 func TestCreateAnalyzeFileResultTable(t *testing.T) {
 	err := createAnalyzeFileResultTable()
 	require.NoError(t, err, "Something messed up")
+}
+func TestAnalyzeFileResultQueryText(t *testing.T) {
+	err := analyzeFileResultQueryText(analyzeFileResultTableName, primaryKeyAttribute{AttributeName: "id", Type: "INTEGER"})
+	require.Equal(t,
+		"CREATE TABLE IF NOT EXISTS TblAnalyzeFileResult (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, FileMetadata INTEGER, Language TEXT, CodeSize INTEGER, CommentSize INTEGER, BlankLines INTEGER, TotalSize INTEGER, FOREIGN KEY (FileMetadata) REFERENCES TblFileMetadata(id))",
+		err,
+		"Something messed up")
 }
 
 func TestInsertRowToFileMetadataTable(t *testing.T) {
