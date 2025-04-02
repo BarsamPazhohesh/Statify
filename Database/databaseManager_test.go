9526c451b34b58b57f4c9ae38feafe00a953401f
rezishon <rezashahmoradi.81@outlook.com>
2025-04-03 02:00:54 +0330

    test: add test for insert data to `AnalyzeFileResultTable`
    This test will run other test functions because it will be an indivial
    unit test. It will create two tables and then insert a row to the
    `FileMetadataTable` because we will test foreign key functionality too.
    At the following, we will test the insert function with sample values.
    First test should goes well & the second test should get error unless
    the foreign key functionality has error.

--- Database/databaseManager_test.go
+++ Database/databaseManager_test.go
@@ -27,6 +27,19 @@ func TestInsertRowToFileMetadataTable(t *testing.T) {
 	err := InsertRowToFileMetadataTable("main", "./statify", "/home/rezishon", ".go", 43, time)
 	require.NoError(t, err, "Something messed up")
 }
+
+func TestInsertRowToAnalyzeFileResultTable(t *testing.T) {
+	TestCreateFileMetadataTable(t)
+	TestCreateAnalyzeFileResultTable(t)
+	TestInsertRowToFileMetadataTable(t)
+
+	err := InsertRowToAnalyzeFileResultTable(1, "go", 25, 10, 6, 41)
+	require.NoError(t, err, "Something messed up")
+
+	err = InsertRowToAnalyzeFileResultTable(6, "go", 25, 10, 6, 41)
+	require.Error(t, err, "Something messed up")
+}
+
 func existenceChecker(t *testing.T) {
 	existence := FileManager.IsFileExists(DatabasePath)
 	//! should check the database table
