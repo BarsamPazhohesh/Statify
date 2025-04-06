9f073ca15689a2a0c1206f34e5b6d3cf17b8fd7f
Rezishon <rezashahmoradi.81@outlook.com>
2025-04-06 15:47:27 +0330

    test(Database)[TestGetAnalyzeFileResult]: add tests
    Three tests just added.
    Also they are concurrent.
    We read data, checks that if we need to add data or not.
    Then they check some attribute searchs.
    Errors, and validity will check too.

--- Database/databaseManager_test.go
+++ Database/databaseManager_test.go
@@ -120,4 +120,40 @@ func TestGetAnalyzeFileResultRows(t *testing.T) {
 }
 
 	assert.NotEqual(t, expect, res)
+func TestGetAnalyzeFileResultRow(t *testing.T) {
+	metadataArray, err := GetAnalyzeFileResultRows()
+	assert.NoError(t, err)
+
+	if len(metadataArray) == 0 {
+		TestInsertRowToAnalyzeFileResultTable(t)
+		metadataArray, err = GetAnalyzeFileResultRows()
+		assert.NoError(t, err)
+	}
+
+	// concurrent
+	t.Run("Test1", func(t *testing.T) {
+		id := metadataArray[len(metadataArray)-1].Id
+		result, err := GetAnalyzeFileResultRow("id", fmt.Sprint(id))
+		assert.NoError(t, err)
+		assert.NotEmpty(t, result)
+	})
+
+	t.Run("Test2", func(t *testing.T) {
+		t.Skip("Doesn't completed")
+		// languageS := metadataArray[len(metadataArray)-1].Language.String()
+		// Analyzer.GetLanguage(metadataArray[len(metadataArray)-1].FileMetadata)
+		// languageS := language.String()
+		//TODO: add function to return lang index(number)
+		result, err := GetAnalyzeFileResultRow("language", fmt.Sprint(0))
+		assert.NoError(t, err)
+		assert.NotEmpty(t, result)
+	})
+
+	t.Run("Test3", func(t *testing.T) {
+		totalSize := metadataArray[len(metadataArray)-1].TotalSize
+		result, err := GetAnalyzeFileResultRow("totalSize", fmt.Sprint(totalSize))
+		assert.NoError(t, err)
+		assert.NotEmpty(t, result)
+	})
+
 }
