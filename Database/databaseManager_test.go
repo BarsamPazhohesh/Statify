2ff32d650bd26fdedc5a23a4d7a240bf038d9aee
Rezishon <rezashahmoradi.81@outlook.com>
2025-04-08 21:18:27 +0330

    chore(Database)[TestAnalyzeFileResultTable]: repalce function

--- Database/databaseManager_test.go
+++ Database/databaseManager_test.go
@@ -12,17 +12,6 @@ import (
 )
 
 
-func TestCreateAnalyzeFileResultTable(t *testing.T) {
-	err := createAnalyzeFileResultTable()
-	require.NoError(t, err, "Something messed up")
-}
-func TestAnalyzeFileResultQueryText(t *testing.T) {
-	query := analyzeFileResultQueryText(analyzeFileResultTableName, primaryKeyAttribute{AttributeName: "id", Type: "INTEGER"})
-	require.Equal(t,
-		"CREATE TABLE IF NOT EXISTS TblAnalyzeFileResult (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, FileMetadataId INTEGER, Language INTEGER, CodeSize INTEGER, CommentSize INTEGER, BlankLines INTEGER, TotalSize INTEGER, FOREIGN KEY (FileMetadataId) REFERENCES TblFileMetadata(id))",
-		query,
-		"Something messed up")
-}
 
 func TestInsertRowToFileMetadataTable(t *testing.T) {
 	TestCreateFileMetadataTable(t)
