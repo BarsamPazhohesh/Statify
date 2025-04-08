9d6b7b6847802f7644aa72e94fa60773eacad4b0
Rezishon <rezashahmoradi.81@outlook.com>
2025-04-08 21:17:56 +0330

    chore(Database)[TestCreateFileMetadataTable]: repalce function

--- Database/databaseManager_test.go
+++ Database/databaseManager_test.go
@@ -11,17 +11,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateFileMetadataTable(t *testing.T) {
-	err := createFileMetadataTable()
-	require.NoError(t, err, "Something messed up")
-}
-func TestFileMetadataQueryText(t *testing.T) {
-	query := fileMetadataQueryText(fileMetadataTableName, primaryKeyAttribute{AttributeName: "id", Type: "INTEGER"})
-	require.Equal(t,
-		"CREATE TABLE IF NOT EXISTS TblFileMetadata (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \n\tName TEXT, \n\tPath TEXT, \n\tDir TEXT, \n\tExtension TEXT, \n\tSize int, \n\tModifiedAt TIMESTAMP)",
-		query,
-		"Something messed up")
-}
 
 func TestCreateAnalyzeFileResultTable(t *testing.T) {
 	err := createAnalyzeFileResultTable()
