fc65f131c0a1bee9d211b83390e45006c297badf
Rezishon <rezashahmoradi.81@outlook.com>
2025-04-08 21:13:27 +0330

    chore(Database)[GetAnalyzeFileResultRow]: repalce function

--- Database/AnalyzeFileResultTable_select.go
+++ Database/AnalyzeFileResultTable_select.go
@@ -51,3 +51,57 @@ func GetAnalyzeFileResultTableRows() ([]Analyzer.AnalyzeFileResult, error) {
 	return results, nil
 }
 
+func GetAnalyzeFileResultTableRow(attributeName string, attributeValue string) (Analyzer.AnalyzeFileResult, error) {
+	var result Analyzer.AnalyzeFileResult
+
+	db, err := sql.Open(driverName, DatabasePath)
+	if err != nil {
+		return Analyzer.AnalyzeFileResult{}, err
+	}
+	defer db.Close()
+
+	row := db.QueryRow(fmt.Sprintf(`
+	SELECT %v.*,
+	%v.id, %v.Language, %v.CodeSize, %v.CommentSize, %v.BlankLines, %v.TotalSize
+	FROM %v JOIN %v ON %v.FileMetadataId = %v.id
+	WHERE %v.%v = '%v'`,
+		fileMetadataTableName,
+		analyzeFileResultTableName,
+		analyzeFileResultTableName,
+		analyzeFileResultTableName,
+		analyzeFileResultTableName,
+		analyzeFileResultTableName,
+		analyzeFileResultTableName,
+		fileMetadataTableName,
+		analyzeFileResultTableName,
+		analyzeFileResultTableName,
+		fileMetadataTableName,
+		analyzeFileResultTableName,
+		attributeName,
+		attributeValue))
+
+	err = row.Scan(
+		&result.FileMetadata.Id,
+		&result.FileMetadata.Name,
+		&result.FileMetadata.Path,
+		&result.FileMetadata.Dir,
+		&result.FileMetadata.Extension,
+		&result.FileMetadata.Size,
+		&result.FileMetadata.ModifiedAt,
+		&result.Id,
+		&result.Language,
+		&result.CodeSize,
+		&result.CommentSize,
+		&result.BlankLines,
+		&result.TotalSize)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			err = fmt.Errorf("%v wasn't valid", attributeName)
+			return Analyzer.AnalyzeFileResult{}, err
+		} else {
+			return Analyzer.AnalyzeFileResult{}, err
+		}
+	}
+
+	return result, nil
+}
